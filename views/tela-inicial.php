bc69d1d8459498dbef50d9319f82dc44c93daf14
AOneManShow <el.mateus727@gmail.com>
2023-06-15 05:37:01 +0100

    Mudado o nome do script da tela inicial

--- views/tela-inicial.php
+++ views/tela-inicial.php
@@ -468,7 +468,7 @@ include_once '../constantes/constantes.php';
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/feather-icons/4.7.3/feather.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js"></script>
-<script type="text/javascript" src="../scripts/custom/scripts.js"></script>
+<script type="text/javascript" src="../scripts/custom/scriptTelaInicial.js"></script>
 
 </body>
 
