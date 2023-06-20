dd6a159c4843c3ac091cbe18eb0e3dcbe6ad025f
AOneManShow <el.mateus727@gmail.com>
2023-06-20 20:27:04 +0100

    Base de dados com os utilizadores mais recentes

--- dbconfig/gestor_tarefas.sql
+++ dbconfig/gestor_tarefas.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 16-Jun-2023 às 18:58
+-- Tempo de geração: 20-Jun-2023 às 20:09
 -- Versão do servidor: 10.4.28-MariaDB
 -- versão do PHP: 8.1.17
 
@@ -24,17 +24,28 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estrutura da tabela `categoria`
+--
+
+CREATE TABLE `categoria` (
+  `idCategoria` int(11) NOT NULL,
+  `nome` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Estrutura da tabela `tarefa`
 --
 
 CREATE TABLE `tarefa` (
   `idTarefa` int(11) NOT NULL,
   `nome` varchar(45) NOT NULL,
-  `descricao` varchar(200) NOT NULL,
-  `idCriador` int(11) NOT NULL,
-  `categoria` varchar(25) NOT NULL,
+  `descricao` varchar(200) DEFAULT NULL,
+  `fkUtilizador` int(11) NOT NULL,
+  `fkCategoria` int(11) NOT NULL,
   `dataInicio` date NOT NULL,
-  `dataConclusao` date NOT NULL,
+  `dataTermino` date NOT NULL,
   `concluida` tinyint(1) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -57,17 +68,22 @@ CREATE TABLE `utilizador` (
 --
 
 INSERT INTO `utilizador` (`idUtilizador`, `nomeCompleto`, `email`, `username`, `senha`) VALUES
-(1, 'Édner Mateus', 'a@gmail.com', 'username', '123'),
-(2, 'Edvino Marvin', 'edmarvino@hotmail.com', 'marvinoofficial', '1234'),
+(1, 'Édner Mateus', 'a@gmail.com', 'edner', 'edner123'),
 (9, 'OMS Man', 'aoms@gmail.com', 'AOneManShow', '1234567'),
-(15, 'Adão Jorge', 'adam12@gmail.com', 'adao', 'abacate'),
 (16, 'Test User', 'testuser@gmail.com', 'Teste', 'teste'),
 (17, 'Milton Dantas', 'milton@gmail.co.ao', 'miltonD', '12345'),
-(18, 'Alberto Mulonde', 'albpedro@gmail.com', 'Xteezy Pvpi', '7654321');
+(18, 'Alberto Mulonde', 'albpedro@gmail.com', 'Xteezy Pvpi', '7654321'),
+(19, 'António Kudiezo', 'kud@gmail.com', 'kud', '12345678');
 
 --
 -- Índices para tabelas despejadas
 --
+
+--
+-- Índices para tabela `categoria`
+--
+ALTER TABLE `categoria`
+  ADD PRIMARY KEY (`idCategoria`);
 
 --
 -- Índices para tabela `tarefa`
@@ -89,6 +105,12 @@ ALTER TABLE `utilizador`
 --
 
 --
+-- AUTO_INCREMENT de tabela `categoria`
+--
+ALTER TABLE `categoria`
+  MODIFY `idCategoria` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT de tabela `tarefa`
 --
 ALTER TABLE `tarefa`
@@ -98,7 +120,7 @@ ALTER TABLE `tarefa`
 -- AUTO_INCREMENT de tabela `utilizador`
 --
 ALTER TABLE `utilizador`
-  MODIFY `idUtilizador` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `idUtilizador` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
